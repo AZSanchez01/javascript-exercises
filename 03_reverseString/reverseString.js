3ff2e5e8eda50dd22e35bc4a413e9793c4e68462
AZSanchez01 <109073612+AZSanchez01@users.noreply.github.com>
2024-10-29 21:07:40 +0800

    feat: Add reverseString function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string = '') {
 
+        let reverseArr = [];
+
+        for (let i = 0; i < string.length; i++) {
+            reverseArr[i] = string.charAt(string.length - i - 1)
+        }
+        return reverseArr.join('');
 };
 
 // Do not edit below this line
