3c37fbd3baa290a811b35f6aab105c8d15d1ee21
AZSanchez01 <109073612+AZSanchez01@users.noreply.github.com>
2024-10-30 11:38:43 +0800

    feat: Add loop to get sum of all numbers between two positive integers in sumAll function. Returns 'ERROR' if non-positive integer values;

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+    if (Number.isInteger(num1) == true && 
+        Number.isInteger(num2) == true &&
+        num1 >= 0 && num2 >=0) {
+        let numHighest = (num1 >= num2)? num1:num2;
+        let numLowest = (num2 < num1)? num2:num1;
+        let sum = numHighest;
+    
+        while (numHighest != numLowest) {
+            numHighest--;
+            sum += numHighest;
+        }
+        return sum;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
