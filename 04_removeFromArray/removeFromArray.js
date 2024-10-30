85eb1def898f5a71308d8dafc222fdf0f5b969b1
AZSanchez01 <109073612+AZSanchez01@users.noreply.github.com>
2024-10-30 11:16:28 +0800

    feat: Add remove elements function using filter.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(givenArray, ...removedElements) {
+
+    function checkRemovedElements (given) {
+        return !(removedElements.includes(given));
+    }
+    return givenArray.filter(checkRemovedElements);
+
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
